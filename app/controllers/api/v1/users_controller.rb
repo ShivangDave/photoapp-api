c3a972d3e7a1d4fa68c747dd86a198ae8fd8b24d
Shivang Dave <daveshivang15@gmail.com>
2020-04-22 15:03:19 -0400

    add tests and statuses..

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,30 +1,47 @@
 class Api::V1::UsersController < ApplicationController
 
-  before_action :find_user, only: [:show, :destroy]
+  before_action :find_user, only: [:show, :update, :destroy]
+  # after_action :check_persistance, only: [:create, :update]
 
   def index
     @users = Api::V1::User.all
-    render :json => @users, :status => :ok
+    if @users
+      render :json => @users, :status => :ok
+    else
+      render :json => @users.error.details, :status => :not_found
+    end
   end
 
   def create
     @user = Api::V1::User.create(users_params)
-    if @user.valid?
-      render :json => @user, :status => :created
-    else
-      # @error = { "error": "failed to create user.."}
-      render :json => @user.error.details, :status => :bad_request
-    end
+    check_persistance
   end
 
   def update
+    @user.update(users_params)
+    check_persistance
   end
 
   def destroy
+    if @user
+      @user.destroy
+      render :json => { :message => "Destroyed.." }, :status => :ok
+    else
+      render :json => { :message => "Failed.." }, :status => :not_found
+    end
   end
 
   private
   def users_params
+    params.require(:user).permit(:username,:password,:profile_name,:email,:location)
+  end
+
+  def check_persistance
+    if @user.persisted?
+      render :json => @user, :status => :ok
+    else
+      render :json => @user.errors.details, :status => :bad_request
+    end
   end
 
   def find_user
