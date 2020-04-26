37db5da638bf9e9fa6e745eb58f499afbcd914f0
Shivang Dave <daveshivang15@gmail.com>
2020-04-26 19:26:16 -0400

    Refactor E2EE and User Auth..

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::API
+    
+    def current_user
+        token = request.headers['Access-Token']
+        user_id = JWT.decode(token,ENV['SUPER_SECRET_USER_KEY'])
+        current = Api::V1::User.find_by(id: user_id)
+    end
+
+    def authenticate!
+        unless current_user
+            return nil
+        end
+    end
+    
     def secure_response(packet, status)
         msg = JWT.encode(packet,ENV['SUPER_SECRET_USER_KEY'])
         render :json => { :message => msg }, :status => status
