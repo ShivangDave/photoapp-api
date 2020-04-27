36d40a568a64371de61b6d487977c6bafbbef4c8
Shivang Dave <daveshivang15@gmail.com>
2020-04-26 21:36:10 -0400

    refactor serializers to accomodate followers and followees..

--- app/models/api/v1/user.rb
+++ app/models/api/v1/user.rb
@@ -1,9 +1,18 @@
 class Api::V1::User < ApplicationRecord
   has_secure_password
 
+  has_many :followed_users, foreign_key: :follower_id, class_name: 'Api::V1::Follow'
+  has_many :followees, through: :followed_users
+
+  has_many :following_users, foreign_key: :followee_id, class_name: 'Api::V1::Follow'
+  has_many :followers, through: :following_users
+
   validates :username, :password, :profile_name, :email, :location, :presence => true
   validates :username, :email, :uniqueness => { :case_sensitive => false }
   validates :email, :email => true
+
+  @VISIBLE_PUBLIC_ATTR = [:username, :profile_name,:location]
+  @VISIBLE_SELF_ATTR = [:username, :profile_name, :location, :email]
 
   def self.profiles
     self.all.as_json(
@@ -12,18 +21,33 @@ class Api::V1::User < ApplicationRecord
         :username,
         :profile_name,
         :location
-      ]
+      ],
+      include: {
+        followers: {
+          only: @VISIBLE_PUBLIC_ATTR,
+          include: :followers
+        },
+        followees: {
+          only: @VISIBLE_PUBLIC_ATTR,
+          include: :followers
+        }
+      }
     )
   end
 
   def profile
     self.as_json(
-      only: [
-        :username,
-        :profile_name,
-        :email,
-        :location
-      ]
+      only: @VISIBLE_SELF_ATTR,
+      include: {
+        followers: {
+          only: @VISIBLE_PUBLIC_ATTR,
+          include: :followers
+        },
+        followees: {
+          only: @VISIBLE_PUBLIC_ATTR,
+          include: :followers
+        }
+      }
     ).merge({ 
       '_id': JWT.encode(
         self.id,
