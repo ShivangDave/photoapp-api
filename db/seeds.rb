4d9d23e2d16eb4e91643b995c8926e7dca643cfb
Shivang Dave <daveshivang15@gmail.com>
2020-04-29 12:01:52 -0400

    Added friendly_id to look up the user using encrypted _id

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,28 @@
 Api::V1::User.destroy_all
 
-u1 = Api::V1::User.create(
-  username: 'test',
-  password: 'test',
-  profile_name: 'test',
-  email: 'test@test.com',
-  location: 'test'
-)
+def seed
+  @u1 = Api::V1::User.create(
+    username: 'test',
+    password: 'test',
+    profile_name: 'test',
+    email: 'test@test.com',
+    location: 'test'
+  )
 
-u2 = Api::V1::User.create(
-  username: 'test2',
-  password: 'test2',
-  profile_name: 'test2',
-  email: 'test2@test.com',
-  location: 'test2'
-)
+  @u2 = Api::V1::User.create(
+    username: 'test2',
+    password: 'test2',
+    profile_name: 'test2',
+    email: 'test2@test.com',
+    location: 'test2'
+  )
 
-# u2 = Api::V1::User.new(
-#   username: 'test',
-#   password: 'test',
-#   profile_name: 'test',
-#   email: 'test@test.com',
-#   location: 'test'
-# )
+  @u1._id = JWT.encode({user_id: @u1.id},ENV['SUPER_SECRET_USER_KEY']).split('.').join('$')
+  @u2._id = JWT.encode({user_id: @u2.id},ENV['SUPER_SECRET_USER_KEY']).split('.').join('$')
+  
+  @u1.save!
+  @u2.save!
+  
+end
+
+seed
