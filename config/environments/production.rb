d19e37e888e2a1aec5e21269b800f03b4fea60d6
Shivang Dave <daveshivang15@gmail.com>
2020-05-04 22:25:47 -0400

    fix heroku config..

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,6 +55,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+
+  config.hosts << "photoapp-api.herokuapp.com"
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
